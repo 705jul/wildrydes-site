deb27804bae7f3dffe9fd0bd78573b953163503f
Juil Kim <705jul@naver.com>
2026-04-14 14:58:09 +0900

    v3

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_hhfzduEgp', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6bq5ok5th10laf4dm36ku2emkf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6bq5ok5th10laf4dm36ku2emkf--1776145422799', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
